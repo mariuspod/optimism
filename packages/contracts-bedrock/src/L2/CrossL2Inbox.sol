a3ba39169abcb2bd6385941bc227407bb938c0ea
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-06 17:25:20 -0800

    Update CrossL2Inbox.sol

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -92,7 +92,7 @@ contract CrossL2Inbox is ISemver {
             tstore(TIMESTAMP_SLOT, calldataload(100))
             tstore(CHAINID_SLOT, calldataload(132))
 
-            if (!call(
+            if (call(
                     gas(), // gas
                     _target, // recipient
                     callvalue(), // ether value
@@ -101,8 +101,7 @@ contract CrossL2Inbox is ISemver {
                     0, // outloc
                     0 // outlen
                 )){
-                    mstore(0x00, 0x3204506f) // 0x3204506f is the 4-byte selector of "CallFailed()"
-                    revert(0x1C, 0x04) // returns the stored 4-byte selector from above
+                    return (0x0, 0)
                 }
         }
     }
