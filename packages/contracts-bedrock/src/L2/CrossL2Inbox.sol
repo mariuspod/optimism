11abe3da3a8cac7c823b429c0bfe2ff53b136d60
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-06 19:02:26 -0800

    contracts-bedrock: fix CrossL2Inbox assembly

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -94,11 +94,6 @@ contract CrossL2Inbox is ISemver {
                     0, // outloc
                     0 // outlen
                 )
-
-            if !success {
-                mstore(0x00, 0xeda86850) // 0xeda86850 is the 4-byte selector of "TargetCallFailed()"
-                revert(0x1C, 0x04) // returns the stored 4-byte selector from above
-            }
         }
     }
 
